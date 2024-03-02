d67cb5bc85f9c48fe2a6a4d791fb62ebcf12b527
Søren Dahl <80645006+Sdahl1234@users.noreply.github.com>
2024-03-02 17:49:55 +0100

    Add files via upload

---
+++ custom_components/aula/const.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+
+STARTUP = """
+                _
+     /\        | |
+    /  \  _   _| | __ _
+   / /\ \| | | | |/ _` |
+  / ____ \ |_| | | (_| |
+ /_/    \_\__,_|_|\__,_|
+Aula integration, version: %s
+This is a custom integration
+If you have any issues with this you need to open an issue here:
+https://github.com/scaarup/aula/issues
+-------------------------------------------------------------------
+"""
+
+DOMAIN = "aula"
+API = "https://www.aula.dk/api/v"
+API_VERSION = "18"
+MIN_UDDANNELSE_API = "https://api.minuddannelse.net/aula"
+MEEBOOK_API = "https://app.meebook.com/aulaapi"
+SYSTEMATIC_API = "https://systematic-momo.dk/api/aula"
+CONF_SCHOOLSCHEDULE = "schoolschedule"
+CONF_UGEPLAN = "ugeplan"
